245de2e5620dc00b501b17d81a7982d67d7827e1
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2024-07-27 00:12:04 -0300

    adiciona variáveis de ambiente para o `pg`
    agora é possível instanciar clients para conexão com o bd sem passar
    argumentos de conexão para o construtor

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,7 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+  const client = new Client();
   await client.connect();
   const result = await client.query(queryObject);
   await client.end();
